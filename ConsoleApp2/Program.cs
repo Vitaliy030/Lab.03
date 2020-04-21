e437c4c9f4649327d457ef96b1ffcdbba8a1cf84
Vitaliy030 <60977465+Vitaliy030@users.noreply.github.com>
2020-04-21 21:55:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader f = new StreamReader("D:\\ІТ\\Програмування\\Програми\\Lab.3\\ConsoleApp2\\111.txt");
+            string s = f.ReadLine();
+            int i = -1;
+            int j = 0;
+
+            string x1;
+            int x0;
+            int sum = 0;
+            int k = 0;
+            foreach (char a in s)
+            {
+                ++i;
+                ++j;
+                if (char.IsWhiteSpace(a) || i == s.Length - 1)
+                {
+                    char[] x = s.ToCharArray(k, j);
+                    x1 = new string(x);
+                    x0 = Convert.ToInt32(x1);
+                    sum += x0;
+                    j = 0;
+                    k = i + 1;
+                }
+            }
+            Console.WriteLine("Сума елементiв масиву: " + sum);
+        }
+    }
+}
